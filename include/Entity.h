9bf22590176fa03f37410bd9fdb66dd853c10a9a
Joachim <joachim11555@gmail.com>
2025-05-24 11:05:56 +0200

    added entities

--- include/Entity.h
+++ include/Entity.h
@@ -3,9 +3,28 @@
 
 class Entity {
     public:
+    Entity(float xPosition_, float yPosition_, float ms_, int health_, int id_)
+     : xPosition(xPosition_), yPosition(yPosition_), movementSpeed(ms_), health(health_), id(id_) {}
+
+    void move(float xNewPosition, float yNewPosition) {xPosition = xNewPosition, yPosition = yNewPosition;}
+    float getPositionX() const {return xPosition;}
+    float getPositionY() const {return yPosition;}
+    float getMovementSpeed() const {return movementSpeed;}
+    int getId() const {return id;}
+    void increaseHealth(int hpGained) {health += hpGained;}
+    void decreaseHealth(int hpLost) {health -= hpLost;}
+    void setHealth(int health_) {health = health_;}
+    void setMovemenetSpeed(float ms) {movementSpeed = ms;}  
+
+    virtual ~Entity() = default;
+
 
     private:
-    
+    int id;
+    int health;
+    float xPosition;
+    float yPosition;
+    float movementSpeed;
 };
 
 
---
+++ src/entities/Player.cpp
@@ -0,0 +1,5 @@
+#include <Player.h>
+
+
+Player::Player(float xPosition, float yPosition, float ms, int health, int id)
+: Entity(xPosition, yPosition, ms, health, id) {}
